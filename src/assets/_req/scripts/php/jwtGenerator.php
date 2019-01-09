6adc80e1c64bd343bd86872d6a966979713e747e
Sohail <sohail.bcs@gmail.com>
2019-01-09 17:10:16 +0530

    jwt with response
    jwt with response

---
+++ src/assets/_req/scripts/php/jwtGenerator.php
@@ -0,0 +1,68 @@
+<?php
+  include_once ("php-jwt-master/src/BeforeValidException.php");
+  include_once ("php-jwt-master/src/ExpiredException.php");
+  include_once ("php-jwt-master/src/SignatureInvalidException.php");
+  include_once ("php-jwt-master/src/JWT.php");
+
+  use \Firebase\JWT\JWT;
+
+  class jwtGenerator{
+    private $key = "8WggZQNvUX6YTDBr3b2UhoGtsdaSCGqlAlGIi4l08mE";
+
+    public function EncodeToken($data){
+      //echo "In jwtTokenSetter";
+      $token = array(
+        "iat" => 1547027358,
+        "data" => $data
+      );
+      echo "\nKey is : ".$this->key."\n";
+      $jwt = JWT::encode($token, $this->key);
+      //print_r($jwt);
+      return $jwt;
+
+    }
+
+    public function IsTokenValid($jwt){
+      // if jwt is not empty
+      if($jwt){
+         // if decode succeed, show user details
+        try {
+             // decode jwt
+            $decoded = JWT::decode($jwt, $this->key, array('HS256'));
+            //print_r($decoded);
+            return true;
+            // set user property values here
+        }
+        // catch failed decoding will be here
+        // if decode fails, it means jwt is invalid
+        catch (Exception $e){
+             // set response code
+          //echo "Token is invalid : ".$e->getMessage();
+          return false;
+            // show error message
+            /*echo json_encode(array(
+                "message" => "Access denied.",
+                "error" => $e->getMessage()
+            ));*/
+        }
+      }
+    // error message if jwt is empty will be here
+    }
+
+    public function DecodeToken($token){
+      $jwt = isset($token) ? $token : "";
+      if ($jwt) {
+        try {
+          $decoded = JWT::decode($jwt, $this->key, array('HS256'));
+          print_r($decoded);
+        }catch (Exception $e){
+          echo "Token is invalid : ".$e->getMessage();
+        }
+      }else{
+        echo "Token is empty";
+      }
+    }
+
+  }
+
+?>
--- src/assets/_req/scripts/php/palika.php
+++ src/assets/_req/scripts/php/palika.php
@@ -16,6 +16,8 @@
 
  include_once ("serverConnector.php");
  include_once ("sessionConn.php");
+ include_once ("jwtGenerator.php");
+
  require 'vendor/autoload.php';
 
 use SparkPost\SparkPost;
@@ -24,7 +26,8 @@ use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
 
 //include("PHPMailer-master/PHPMailerAutoload.php");
 //echo"palika";
-class palika extends connector{
+class palika extends connector {
+  protected $response = array();
 
   public function userAdaptor($operation, $value){
     if($value == "" || $value == "" || $value == NULL){
@@ -40,13 +43,14 @@ class palika extends connector{
     }
 
   public function signup($value){
+    $this->clearOldResponseData();
     $oBasic = new basic();
     $ifUser = $oBasic->userAdaptor('checkIfEmailExists', md5($value['email']));
-    echo "ifUser >> ".$ifUser."::";
+    //echo "ifUser >> ".$ifUser."::";
     if($ifUser == "false"){
       $result = "";
       $simple_email = $value['email'];
-      echo "Simple email >> ".$simple_email." ::";
+      //echo "Simple email >> ".$simple_email." ::";
       $value['email'] = md5($value['email']);
       $value['password'] = md5($value['password']);
       //echo $value['password'];
@@ -55,43 +59,59 @@ class palika extends connector{
 
       if($value['email'] != "" && $value['password'] != ""){
         $query = "INSERT INTO user_instance (email, pass, veri_id) VALUES(?, ?, ?)";
-        echo vsprintf( str_replace("?","'%s'",$query),$value);
+        //echo vsprintf( str_replace("?","'%s'",$query),$value);
         $result = $this->query_db($query, $value);
         //echo"query executed";
-        echo "Result :".$result;
+        //echo "Result :".$result;
         if($result == 1){
-          $oSession = new sessionExr();
-          $ans = $oSession->sessionEmailSetter($simple_email);
-          echo $ans;
+          /*$oSession = new sessionExr();
+          $ans = $oSession->sessionEmailSetter($simple_email);*/
+          $jwtObj = new jwtGenerator();
+          $jwt = $jwtObj->EncodeToken(array('email'=>$simple_email));
+          $this->spark($simple_email);
+          $response['response'] = "true";
+          $response['errMessage'] = "";
+          $response['token'] = $jwt;
+          //echo $ans;
           //return $ans;
           //echo "ANS >> ".$ans."::";
-          if($ans == "true"){
+          //if($ans == "true"){
             //echo"if executed";
-            $this->spark($simple_email);
+            //$this->spark($simple_email);
             // $file_con = file_get_contents("../../../conf_email.html");
             // $file_con = str_replace("sent", $unq, $file_con);
             // echo $this->spark($simple_email,$file_con);
             //echo $this->sendVEmail($simple_email, $file_con);
-          }else{
-            echo "Ans  >> ".$ans." ::";
+          //}else{
+            //echo "Ans  >> ".$ans." ::";
             //echo $ans;
-            return $ans;
-          }
+            return $response;
+          //}
         }else{
-          return "efalse";
+          //return "efalse";
+          $response['response'] = "efalse";
+          $response['errMessage'] = "Error inserting user.";
+          return $response;
         }
         $this->db_close();
       }else{
-        return "bfalse";
+        //return "bfalse";
+        $response['response'] = "bfalse";
+        $response['errMessage'] = "Blank Username or Password";
+        return $response;
       }
     }else{
-      return "rfalse";
+      //return "rfalse";
+      $response['response'] = "rfalse";
+      $response['errMessage'] = "User already exist.";
+      return $response;
     }
   }
 
   public function login($value){
     //$this->db_connection();
     //echo "in login function";
+    $this->clearOldResponseData();
     $oBasic = new basic();
     $ifUser = $oBasic->userAdaptor('checkIfEmailExists', md5($value['email']));
     if($ifUser == "true"){
@@ -100,18 +120,40 @@ class palika extends connector{
       $result = $this->query_db($query, $email);
       $result = mysqli_fetch_array($result);
       if($result['pass'] == substr(md5($value['password']), 0, 50)){
-        $oSession = new sessionExr();
-        $ans = $oSession->sessionEmailSetter($value['email']);
+        /*$oSession = new sessionExr();
+        $ans = $oSession->sessionEmailSetter($value['email']);*/
+        $jwtObj = new jwtGenerator();
+        $jwt = $jwtObj->EncodeToken(array('email'=>$value['email']));
+        //print_r($jwt);
+        //echo "\nToken is : ".$jwtObj->IsTokenValid($jwt)."\n";
+        //$jwtObj->DecodeToken($jwt);
         $this->db_close();
-        return $ans;
+        $response['response'] = "true";
+        $response['errMessage'] = "";
+        $response['token'] = $jwt;
+        //return $ans;
+        $return $response;
       }else{
         //return "pfalse >> ".md5($value['password'])." :: ".$result['pass'];
-        return "pfalse";
+        //return "pfalse";
+        $response['response'] = "pfalse";
+        $response['errMessage'] = "Password mismatch.";
+        $return $response;
       }
     }else {
-      return "ufalse";
+      //return "ufalse";
+      $response['response'] = "ufalse";
+      $response['errMessage'] = "Invalid Username !!!";
+      $return $response;
     }
   }
+
+  public function clearOldResponseData(){
+    unset($response);
+    $response = array();
+  }
+
+
 
   // $email = strval($value['email']);
   //echo gettype($abc);
